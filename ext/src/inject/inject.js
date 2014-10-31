fdb7ea6b6d0365eb727bfaa8b259e17e107e42d3
OpenBijbel.nu Ontwikkelaar <Openbijbel.nu@gmail.com>
2014-10-31 18:55:09 +0100

    Update inject.js

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,4 +1,23 @@
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
 
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+var myPrettyCode = function() {
+     alert("success");
+};
 
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
@@ -9,15 +28,8 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("This message was sent from scripts/inject.js");
 		alert("this is from NIV-reftagger in inject.js");
-		jQuery(function ($) { // First argument is the jQuery object
-		    // Do stuff to the DOM
-    			$('body').append('<p>Hello World!</p>');
-    			$(document).ready(function() { 
-				$.getScript("https://raw.githubusercontent.com/Openbijbel-nu/reftaggerdemo/master/reftagger-niv.js");
-			}); 
-		});
+		loadScript("https://raw.githubusercontent.com/Openbijbel-nu/reftaggerdemo/master/reftagger-niv.js", myPrettyCode);
 		// ----------------------------------------------------------
 	}
 	}, 10);
-	alert("this is from NIV-reftagger in inject.js 2");
 });
