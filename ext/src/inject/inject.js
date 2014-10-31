d62b28e0fbd047dc98f1b6b0dc01168e9bb936da
OpenBijbel.nu Ontwikkelaar <Openbijbel.nu@gmail.com>
2014-10-31 18:48:40 +0100

    Update inject.js

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -9,10 +9,13 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("This message was sent from scripts/inject.js");
 		alert("this is from NIV-reftagger in inject.js");
-		$(document).ready(function() { 
-
-			$.getScript("https://raw.githubusercontent.com/Openbijbel-nu/reftaggerdemo/master/reftagger-niv.js");
-		}); 
+		jQuery(function ($) { // First argument is the jQuery object
+		    // Do stuff to the DOM
+    			$('body').append('<p>Hello World!</p>');
+    			$(document).ready(function() { 
+				$.getScript("https://raw.githubusercontent.com/Openbijbel-nu/reftaggerdemo/master/reftagger-niv.js");
+			}); 
+		});
 		// ----------------------------------------------------------
 	}
 	}, 10);
